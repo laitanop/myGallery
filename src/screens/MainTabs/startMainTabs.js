3a149b9d6ca5a0d9f74e78819c08f93b3e1e8dd8
Pamela Rivera <pamelarivera@MacBook-Pro-de-Pamela.local>
2018-09-16 13:33:16 -0600

    adding icons to tabs

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,21 +1,32 @@
 import { Navigation } from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 
 const startTabs = () => {
-    Navigation.startTabBasedApp({
-        tabs: [
-            {
-                screen: "myGallery.FindPlaceScreen", 
-                label: "Find Place",
-                title: "Find Place"
-            }, 
-            {
-                screen: "myGallery.SharePlaceScreen", 
-                label: "Share Place",
-                title: "Share Place"
-            }
-        ]
-    })
+    Promise.all([
+        Icon.getImageSource("md-map", 30),
+        Icon.getImageSource("ios-share-alt", 30)
+    ]).then(sources => {
+        Navigation.startTabBasedApp({
+            tabs: [
+                {
+                    screen: "myGallery.FindPlaceScreen", 
+                    label: "Find Place",
+                    title: "Find Place", 
+                    icon: sources[0]
+                }, 
+                {
+                    screen: "myGallery.SharePlaceScreen", 
+                    label: "Share Place",
+                    title: "Share Place",
+                    icon: sources[1]
+                }
+            ]
+        })
+    });
+
+ 
 }
  
 export default startTabs;
